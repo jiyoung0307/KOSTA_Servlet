27a92c1d27e184f95d937b5932782776bf2b586d
zy_zero <jiyoungjang0307@gmail.com>
2023-10-18 17:08:46 +0900

    첫 커밋

---
+++ src/main/java/ch04/CalcServlet.java
@@ -0,0 +1,27 @@
+package ch04;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/calc")
+public class CalcServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    int n1 = Integer.parseInt(req.getParameter("n1"));
+    int n2 = Integer.parseInt(req.getParameter("n2"));
+    String op = req.getParameter("op");
+    System.out.println(n1 + op + n2);
+  }
+
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.getWriter().append("Served at : ").append(req.getContextPath());
+    doGet(req, resp);
+  }
+
+}
